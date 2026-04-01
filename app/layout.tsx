0bab50aea341c1e9d2c8449806eeeb07503f38ee
Stuart Roper <stuart.roper@bravissimo.com>
2026-04-01 12:33:59 +0100

    Initial commit - product preview card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat, Fraunces } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
+  weight: ["500", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const fraunces = Fraunces({
+  variable: "--font-fraunces",
   subsets: ["latin"],
+  weight: ["700"],
 });
 
 export const metadata: Metadata = {
@@ -24,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${montserrat.variable} ${fraunces.variable}`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,64 +3,25 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className={styles.container}>
+        <div className={styles.card}>
+            <div className={styles.cardImage}>
+                <Image src="/images/image-product-desktop.jpg" fill alt="Product Image" />
+            </div>
+            <div className={styles.productDetails}>
+                <p className={styles.label}>PERFUME</p>
+                <h1 className={styles.headingPrimary}>Gabrielle Essence Eau De Parfum</h1>
+                <p className={styles.bodyText}>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the
+                    House of CHANEL.</p>
+                <div className={styles.priceContainer}>
+                    <span className={styles.priceCurrent}>$149.99</span>
+                    <span className={styles.priceOriginal}>$169.99</span>
+                </div>
+                <button className={styles.button}>
+                  <Image src="/images/icon-cart.svg" width={24} height={24} alt="Cart Icon" />
+                  <div className={styles.buttonText}>Add to Cart</div></button>
+            </div>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    </main>
   );
 }
